083a482092f358ad3d9fc4a864b376594c4a3125
Paulo Sérgio <74740116+paulosdsm1@users.noreply.github.com>
2023-09-01 13:34:35 -0300

    Atualização 01/09/2023

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [{
+    "id": "1",
+    "title": "Saiu novo filme",
+    "description": "Olá Mundo",
+    "photoCover": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"
+},
+
+{
+    "id": "2",
+    "title": "Saiu novo filme",
+    "description": "Olá Mundo",
+    "photoCover": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"
+}
+
+]
